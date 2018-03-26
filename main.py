852e678aed9b365f1eaeb646865ffdedf566f8ac
xyzdh <309502059@qq.com>
2018-03-26 21:00:14 +0800

    Revert "main入口文件"
    This reverts commit 7d0d0dbd883466b867bc6ac2cdea5be9f9e79e45.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from kivy.app import App
+<<<<<<< HEAD
 from kivy.uix.button import Button
+=======
+from kivy.uix.button import Button
+class NonePy(App):
+    """NonePy start"""
+    def build(self):
+        return Button(text = "Hello World")
+
+if __name__ == '__main__':
+    NonePy().run()
+>>>>>>> parent of 51a0aea... Revert "main入口文件"
