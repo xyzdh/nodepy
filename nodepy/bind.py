a8e071cd2ef180cb06a3d57fedf83e88b92c8856
xyzdh <309502059@qq.com>
2018-04-11 20:58:06 +0800

    Bind()改为 只检测 一层
    Bind()改为 只检测 一层。不会 对 list 的 方法 变动 作出 反应

--- nodepy/bind.py
+++ nodepy/bind.py
@@ -1,34 +1,48 @@
-#糟糕的实现，无法 对 深层的 变化 引起 的 整体 变化 作出相应。弃用
 '''var = Bind()
 当 对象 的 值 改变时，作出 反应。
-When the value of Object is changed,do something.'''
-
+When the value of Object is changed,do something.
+只对 list/dict/class 一层 作出反应.
+不会 对 list的方法作出反应(eg:test.remove(2))
 '''
-在 同一行 的 多次 变化 只会 callback一次,因此 尽量 避免 一行 多次使用。
-最好的写法是一行 只 处理一项。
-eg:
-    test = Bind()
-    def call(value,old_value):
-        print(old_value,'---old')
-    def condition(value,old_value):
-        if isinstance(value,int) and value > 6:
-            print('will not trigger call,value == ',value)
-            return False
-        return True
+'''
+test = Bind()
 
-    test.bind(call,condition)
+def call(value,old_value):
+    print(old_value,'---old')
+def condition(value,old_value):
+    if isinstance(value,int) and value > 6:
+        print('will not trigger call,value == ',value)
+        return False
+    return True
+#绑定 动作 及 条件
+test.bind(call,condition)
 
-    test(6) # test() == 6 #None ---old
-    test(7) # test() == 7
+#普通 赋值
+test(6) # test() == 6 #None ---old
+test(7) # test() == 7
 
-    test([1,2,3])# test() == 123 #6 ---old
-    test()[1] = 6 # test() == [1,6,3] #[1, 2, 3] ---old
+test([1,2,3])# test() == 7 #7 ---old
+# 给子项赋值，可以 检测 到 变动(仅限一层)
+test[1] = 6 #test() == [1,6,3]
+#打印 值
+print(test) # == print(test()) # [1,6,3]
+#将 值 传递给 其他对象.失去 检测变动 的 功能
+t = test() # t == [1,6,3]
+t[0] = 8
+####################
+class Temp():
+    www = 'Hello'
 
-    test.unbind(call) # unbind Callback & its Condition 
+test(Temp())
+print(test.www) #Hello
+test.www = 'World'#检测到 变动
 
-    test('nothing will print')
+#解绑。动作与条件 是 一起的
+test.unbind(call) # unbind Callback & its Condition 
 
-    print(test())
+test('nothing will print')
+
+# print(test)
 '''
 
 from types import FunctionType, MethodType
@@ -42,95 +56,51 @@ from codecs import open
 class Bind():
 
     def __init__(self, value=None, cache=True):
-        self.__dict__ = {'_Bind__value': value, '_Bind__old_value': None,
-                         '_Bind__callback': [], '_Bind__condition': [], '_Bind__cache': {}}
-        self.__setattr__ = self.__setitem__
+        self.__value = value
+        self.__old_value = None
+        self.__callback = []
+        self.__condition = []
+
+    def __setattr__(self,name,value):
+        if not name.startswith('_Bind__'):
+            exec("self._Bind__value."+name+'=value')
+            self.__value_change_call()
+            return
+        self.__dict__[name] = value
 
     def __getattr__(self, name):
-        print(46546,name,88888888)
-        return eval("self.__dict__['_Bind__value']."+name)
 
-    def __getitem__(self, name):
-        return self.__dict__['_Bind__value'][name]
-        # eval('self.__value['+name+']')
+        return eval("self._Bind__value."+name)
 
     def __setitem__(self, name, value):
-
-        frame = _getframe()
-        filename = frame.f_back.f_code.co_filename
-        lineno = frame.f_back.f_lineno
-
-        _local = frame.f_back.f_locals
-        _global = frame.f_back.f_globals
-        var = {}
-    
-        with open(filename, "r", encoding='UTF-8') as file:
-            source = file.readlines()[lineno - 1]
-            source = "".join(source.split())#去除空格,换行符,制表符
-
-            if source[-1] != ':':  # 说明不是 if/while/for…… 之类
-                nodes = parse(source)
-
-                for node in walk(nodes):
-                    if isinstance(node, Name):
-                        _id = node.id
-
-                        if var.get(_id):  # 不重复处理相同的name
-                            continue
-
-                        try:
-                            var[_id] = _local[_id]
-                        except:
-                            var[_id] = _global[_id]
-
-
-                        exclude = []#剔除 字符串
-                        for w in ['"(.+?)"',"'(.+?)'"]:
-                            exclude += [[m.start(),m.end()] for m in finditer(w, source)]
-
-                        start,length =  0,len(_id)
-                        inside = lambda x,y:y[0]<=x<=y[1]
-                        while 1:
-                            start = source.find(_id, start)
-                            if start == -1:
-                                break
-
-                            t = start+length
-                            if all([not inside(start, r) for r in exclude]):
-                                    source = source[:t]+".__dict__['_Bind__value']"+source[t:]
-                            start = t
-        print(source,2222222)
-        exec(source, {}, var)
-        # self.__dict__['_Bind__value'][name] = value
+        self.__dict__['_Bind__value'][name] = value
         self.__value_change_call()
+        
+    def __getitem__(self, name):
+        return self.__value[name]
 
-    # __setattr__ = __setitem__
-    # # def __delitem__(self,key):
-    # def __delitem__(self,key):
-    #     print(key,6546546)
     def __value_change_call(self):
-        value = self.__dict__['_Bind__value']
-        old_value = self.__dict__['_Bind__old_value']
+        value = self.__value
+        old_value = self.__old_value
+
         if value == old_value:
             return
 
         v = {'value': value, 'old_value': old_value}
-        for index, condition in enumerate(self.__dict__['_Bind__condition']):
+        for index, condition in enumerate(self.__condition):
             run_call = (condition is None or condition(**v))
 
             if run_call:
-                self.__dict__['_Bind__callback'][index](**v)
+                self.__callback[index](**v)
 
         self.__dict__['_Bind__old_value'] = deepcopy(value)
 
     def __call__(self, *args):
-        # print(self.__dict__)
         num = len(args)
         if num == 0:
-            return self.__dict__['_Bind__value']  
+            return self.__value
 
         if num == 1:
-            # print('22222222')
             self.__dict__['_Bind__value'] = args[0]
             self.__value_change_call()
             return
@@ -140,7 +110,7 @@ class Bind():
                 "takes 1 positional arguments but %d were given" % len(args))
 
     def __str__(self):
-        return str(self.__dict__['_Bind__value'])
+        return str(self.__value)
 
     __repr__ = __str__
 
@@ -154,64 +124,37 @@ class Bind():
             raise TypeError("can not call")
         if ifs != None and not self.__check(ifs):
             raise TypeError("can not call")
-        if call not in self.__dict__['_Bind__callback']:  # 相同 的 call 只会 绑定一次
-            self.__dict__['_Bind__callback'].append(call)
-            self.__dict__['_Bind__condition'].append(ifs)
+        if call not in self.__callback:  # 相同 的 call 只会 绑定一次
+            self.__callback.append(call)
+            self.__condition.append(ifs)
 
     def unbind(self, call):
         if not self.__check(call):
             raise TypeError("can not call")
         try:
-            index = self.__dict__['_Bind__callback'].index(call)
-            del self.__dict__['_Bind__callback'][index]
-            del self.__dict__['_Bind__condition'][index]
+            index = self.__callback.index(call)
+            del self.__callback[index]
+            del self.__condition[index]
         except ValueError:
             raise 'unbind fail.The call is not a Bind callback'
 
+
 test = Bind()
 
-
-def call(value, old_value):
-    print(old_value, '---old')
-
-
-def condition(value, old_value):
-    if isinstance(value, int) and value > 6:
-        print('will not trigger call,value == ', value)
+def call(value,old_value):
+    print(old_value,'---old')
+def condition(value,old_value):
+    if isinstance(value,int) and value > 6:
+        print('will not trigger call,value == ',value)
         return False
     return True
-
+#绑定 动作 及 条件
 test.bind(call,condition)
 
-test([1, [2,3]])  # test() == 6 #None ---old
-test[1] = 6
-print(test) 
+#普通 赋值
+test(6) # test() == 6 #None ---old
+test(7) # test() == 7
 
-# class AA():
-#     wow = 6
-# test(AA)
-# test.wow = 7
-# print(test.wow)
-
-# print(test[-1])
-# test[1] = 3
-# print(test)
-# c = [12,3]
-# # del test[1],c[1]
-# # print(test,c)
-# test[1] = [5,6]
-# test[1][0] = 8
-# print(test)
-# if test == [1,3]:
-#     print(56465)
-# test(7) # test() == 7
-
-# test([1,2,3])# test() == 123 #6 ---old
-# test()[1] = 6 # test() == [1,6,3] #[1, 2, 3] ---old
-
-# test.unbind(call) # unbind Callback & its Condition
-
-# test('nothing will print')
-
-# print(test())
-# print(test._Bind__value)
+test([1,2,3])# test() == 7 #7 ---old
+test.remove(2)
+print(test)
